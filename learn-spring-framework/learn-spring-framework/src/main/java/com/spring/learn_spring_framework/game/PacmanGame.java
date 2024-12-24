44d9f53edd8ed349fb51e663fc9e03cf95ea0a91
ayush <mishraayush9991@gmail.com>
2024-12-24 17:18:03 +0530

    Iteration 2 changes.

--- learn-spring-framework/learn-spring-framework/src/main/java/com/spring/learn_spring_framework/game/PacmanGame.java
+++ learn-spring-framework/learn-spring-framework/src/main/java/com/spring/learn_spring_framework/game/PacmanGame.java
@@ -1,5 +1,25 @@
 package com.spring.learn_spring_framework.game;
 
-public class PacmanGame {
+public class PacmanGame implements GamingConsole {
+
+	@Override
+	public void down() {
+		System.out.println("down");
+	}
+
+	@Override
+	public void left() {
+		System.out.println("left");
+	}
+
+	@Override
+	public void right() {
+		System.out.println("right");
+	}
+
+	@Override
+	public void up() {
+		System.out.println("up");
+	}
 
 }
